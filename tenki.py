99577538febe90fb00613cc833b08661dad079f7
algon-320 <algon.0320@gmail.com>
2017-05-31 16:12:18 +0900

    オプションを追加

--- tenki.py
+++ tenki.py
@@ -6,11 +6,12 @@ from modules.weather_forecast_manager import WeatherForecastManager
 
 
 @click.command()
-
-def tenki():
-    # つくば市の天気
-    wfm = WeatherForecastManager('http://www.tenki.jp/forecast/3/11/4020/8220.html')
-    wfm.print_weather(WeatherForecastManager.SHOW_ALL)
+@click.option('--url', type=str, default='http://www.tenki.jp/forecast/3/11/4020/8220.html'
+                help='ピンポイント天気(3時間天気)のページのURL') # つくば市の天気
+@click.option('--conky' is_flag=True, help='Conkyに表示させるときに指定してください')
+def tenki(url, conky):
+    wfm = WeatherForecastManager(url)
+    wfm.print_weather(WeatherForecastManager.SHOW_ALL, conky=conky)
 
 if __name__ == '__main__':
     tenki()
