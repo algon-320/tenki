92580a451101ab4b1dd856016493be35fae3bb8f
algon-320 <algon.0320@gmail.com>
2019-06-03 15:27:25 +0900

    slimモードを追加

--- tenki.py
+++ tenki.py
@@ -18,9 +18,17 @@ from modules.weather_forecast_manager import WeatherForecastManager
               help='3時間天気のページのURL') # つくば市の天気
 @click.option('--days', type=int, default=2, help='表示させる日数[1~3] (デフォルト2)')
 @click.option('--conky', is_flag=True, help='Conkyに表示させるときに指定してください')
-def tenki(url, days, conky):
+@click.option('--slim', is_flag=True, help='湿度、風速を表示しない')
+def tenki(url, days, conky, slim):
     wfm = WeatherForecastManager(url)
-    wfm.print_weather(WeatherForecastManager.SHOW_ALL, conky=conky, days=days)
+    if slim:
+        show = \
+            WeatherForecastManager.SHOW_WEATHER | \
+            WeatherForecastManager.SHOW_TEMPERATURE | \
+            WeatherForecastManager.SHOW_PROBABILITY_OF_RAIN
+        wfm.print_weather(show, conky=conky, days=days)
+    else:
+        wfm.print_weather(WeatherForecastManager.SHOW_ALL, conky=conky, days=days)
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(__file__))
